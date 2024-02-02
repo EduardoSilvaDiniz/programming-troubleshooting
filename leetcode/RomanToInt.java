8aff7b925c097aa50c8cc60767cbc0f824e925b6
Eduardo da Silva Diniz <eduardofox989@protonmail.com>
2024-02-02 17:10:37 -0300

    update codes

--- leetcode/RomanToInt.java
+++ leetcode/RomanToInt.java
@@ -1,11 +1,28 @@
 import java.util.HashMap;
 
 public class RomanToInt {
-    public static int romanToInt(String s)
-    {
-        final HashMap<Character, Integer> map = new HashMap<>();
-        int result = 0;
+ public static int romanToInt(String s) {
+        //TODO usar API lambda para simplificar o codigo
+        if (s.isEmpty())
+            throw new IllegalArgumentException("String nula");
 
+        String roman = "IVXLCDM";
+        int equals = 0;
+
+        for (char c : s.toCharArray())
+        {
+            for (char r : roman.toCharArray())
+            {
+                if(c == r)
+                    equals++;
+            }
+        }
+
+        if (!(equals == s.length()))
+            throw new IllegalArgumentException("String não contem numeros romando ou contem chars que não são numeros romanos");
+
+        final HashMap<Character,Integer> map = new HashMap<Character, Integer>();
+        int result = 0;
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
@@ -16,10 +33,11 @@ public class RomanToInt {
 
         for (int i = 0; i < s.length(); i++)
         {
-            if ((i < (s.length()-1)) && (map.get(s.charAt(i)) < map.get(s.charAt(i+1))))
+            if (i+1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
                 result -= map.get(s.charAt(i));
-            else
+            } else {
                 result += map.get(s.charAt(i));
+            }
         }
         return result;
     }
